2136b75fb3b89e09b85d37b04d539f7ec31e44a3
Cagatay Yildiz <yildizc1@cs-209.org.aalto.fi>
2019-06-04 13:15:25 +0300

    gif updated

--- scripts/test_mnist_nonuniform.sh
+++ scripts/test_mnist_nonuniform.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python test.py --task mnist_nonuniform --ckpt checkpoints/mnist_nonuniform/mnist_nonuniform_q8_inst1_fopt2_enc8_dec12
-# python test.py --task mnist_nonuniform --ckpt /Users/yildizc1/Downloads/checkpoints/mnist_nonuniform/neurips-results
+# python test.py --task mnist_nonuniform --ckpt checkpoints/mnist_nonuniform/neurips-results
